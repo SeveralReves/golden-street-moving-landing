4c4756f153866c005743d801a172bd15331e3973
Gabo Duran <gabrielalexander2@gmail.com>
2025-10-20 02:58:26 -0400

    feat: add hero

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,28 @@
+@php
+  $image = Vite::asset('resources/images/hero.png');
+    
+@endphp
+
+<div class="hero">
+  <picture class="hero__picture">
+    <img src="{{ $image }}" alt="golden hero moving" title="golden hero moving"  loading="lazy" class="hero__image">
+  </picture>
+  <div class="hero__overlay"></div>
+  <div class="hero__content container">
+      @if (isset($title) && !empty($title))
+        <h1 class="hero__title">
+          {{$title}}
+        </h1>
+      @endif
+      @if (isset($description) && !empty($description))
+        <p class="hero__description lead">
+          {{ $description }}
+        </p>  
+      @endif
+      @if (isset($button['url']) && !empty($button['url']))
+        <a href="{{ $button['url'] }}" title="{{ $button['title'] ?? 'Ver más' }}" class="hero__button button__primary">
+          {{ $button['title'] ?? 'Ver más' }}
+        </a>
+      @endif
+  </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,22 @@
 
 @section('content')
 
-<div class="container">
+    @include('components.hero', [
+        'title' => 'Your move is easy, safe, and on time.',
+        'description' => 'We are a professional moving service that ensures a stress-free experience from start to finish. Our team of experts handles everything with care and precision.',
+        'button' => [
+            'url' => '#',
+            'title' => 'Book your date now'
+        ],
+    ])
+
+{{-- <div class="container">
     <p>This is the user content</p>
     
-        {{-- Isla Vue con props JSON --}}
     <div
         data-vue="ExampleComponent"
         data-props='@json(["postId" => 123, "initial" => false])'>
     </div>
 
-</div>
+</div> --}}
 @stop
